a5d69fe4c8155df665f4ee641fca8f849b696067
Yusuke Kato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2016-09-30 14:42:08 +0000

    ひとまず完成

--- chess_robot.c
+++ chess_robot.c
@@ -1,18 +1,18 @@
 /**
  * 9.26.2016
- * 9.29.2016
+ * 9.30.2016
  * Chess Robot
  * Yusuke Kato
  */
  
 /**
- * 1.人対人で対戦できるチェス作成	-> できる
- * 2.chessAI作成					-> ルールに従って動かせるくらいでいい
+ * 1.人対人で対戦できるチェス作成 -> 完成
+ * 2.chessAI作成
  * 3.現実の座標を取り入れて、
  *   ロボットハンドで駒を動かせるようにする
- *   生体工学 逆運動学				-> 授業で学ぶ
+ *   生体工学 逆運動学
  * 4.上にカメラをつけて画像処理で
- *   プレイヤーの駒の動きを読み取る	-> 授業で学ぶ
+ *   プレイヤーの駒の動きを読み取る
  */
  
 /**
@@ -51,6 +51,7 @@ char before;	/* 移動前の駒 */
 char after;		/* 移動後の駒 */
 int turn_p1 = 1;	/* ターンフラグ player1 */
 int turn_p2 = 1;	/* ターンフラグ player2 */
+int flag_turn = 1;	/* player1: 1  player2: 2 */
 
 /* 盤面を出力 */
 void print_board(void)
@@ -72,7 +73,11 @@ int input(void)
 	int i;
 	
 	/* 駒の移動を指定する文字列入力 */
-	printf("\n (h:help)入力 : ");
+	if(flag_turn == 1) {
+		printf("\n (player1)入力 : ");
+	} else if(flag_turn == 2) {
+		printf("\n (player2)入力 : ");
+	}
 	gets(input_char);
 	
 	/* 終了コマンド */
@@ -97,26 +102,76 @@ int input(void)
 	before = board[input_int[0]][input_int[1]];	/* 移動前の駒 */
 	after = board[input_int[2]][input_int[3]];	/* 移動後の駒 */
 	
-	/* 駒以外を指定したら無効 */
-	if(!(before == 'P' ||
-		 before == 'K' ||
-		 before == 'Q' ||
-		 before == 'B' ||
-		 before == 'N' ||
-		 before == 'R' )) {
-		 printf(" 駒を指定できていません\n");
-		 return 2;	/* 例外終了 */
+	if(flag_turn == 1) {
+		/* 駒以外を指定したら無効 */
+		if(!(before == 'P' ||
+		 	before == 'K' ||
+		 	before == 'Q' ||
+		 	before == 'B' ||
+		 	before == 'N' ||
+		 	before == 'R' )) {
+		 	printf(" 駒を指定できていません\n");
+		 	return 2;	/* 例外終了 */
+		}
+	} else if(flag_turn == 2) {
+		/* 駒以外を指定したら無効 */
+		if(!(before == 'p' ||
+		 	before == 'k' ||
+		 	before == 'q' ||
+		 	before == 'b' ||
+		 	before == 'n' ||
+		 	before == 'r' )) {
+		 	printf(" 駒を指定できていません\n");
+		 	return 2;	/* 例外終了 */
+		}
 	}
 	
-	/* 移動先に自分の駒があったら無効 */
-	if( after == 'P' ||
-		after == 'K' ||
-		after == 'Q' ||
-		after == 'B' ||
-		after == 'N' ||
-		after == 'R' ) {
-		printf( " 駒の移動先に自分の駒があります\n");
-		return 2;	/* 例外終了 */
+	if(flag_turn == 1) {
+		/* 移動先に自分の駒があったら無効 */
+		if( after == 'P' ||
+			after == 'K' ||
+			after == 'Q' ||
+			after == 'B' ||
+			after == 'N' ||
+			after == 'R' ) {
+			printf( " 駒の移動先に自分の駒があります\n");
+			return 2;	/* 例外終了 */
+		}
+	} else if(flag_turn == 2) {
+		/* 移動先に自分の駒があったら無効 */
+		if( after == 'p' ||
+			after == 'k' ||
+			after == 'q' ||
+			after == 'b' ||
+			after == 'n' ||
+			after == 'r' ) {
+			printf( " 駒の移動先に自分の駒があります\n");
+			return 2;	/* 例外終了 */
+		}
+	}
+	
+	/*player2の駒を判定の時だけ大文字にする */
+	switch(before){
+		case 'p':
+			before = 'P';
+			break;
+		case 'k':
+			before = 'K';
+			break;
+		case 'q':
+			before = 'Q';
+			break;
+		case 'b':
+			before = 'B';
+			break;
+		case 'n':
+			before = 'N';
+			break;
+		case 'r':
+			before = 'R';
+			break;
+		default:
+			break;
 	}
 	
 	return 0;	/* 正常終了 */
@@ -125,20 +180,30 @@ int input(void)
 /* 駒の移動が正しいかチェック */
 int move_check(void)
 {
-	int i;	/* for文用 */
+	int i, j;	/* for文用 */
 	
 	switch (before) {
 		/* ポーン 一つ進む（始め二つ進む） */
 		case 'P':
+			/* 列が等しくない場合 */
 			if(input_int[1] != input_int[3]) {
 				printf(" 列の入力が不正です\n");
 				return 2;	/* 例外終了 */
 			}
-			if(turn_p1 == 1 && (input_int[2] == (input_int[0] - 2))) {
-				return 0;	/* 正常終了 */
-			} else if(input_int[2] != (input_int[0] - 1)) {
-				printf(" 行の入力が不正です\n");
-				return 2;	/* 例外終了 */
+			if(flag_turn == 1) {
+				if(turn_p1 == 1 && (input_int[2] == (input_int[0] - 2))) {
+					return 0;	/* 正常終了 */
+				} else if(input_int[2] != (input_int[0] - 1)) {
+					printf(" 行の入力が不正です\n");
+					return 2;	/* 例外終了 */
+				}
+			} else if(flag_turn == 2) {
+				if(turn_p2 == 1 && (input_int[2] == (input_int[0] + 2))) {
+					return 0;	/* 正常終了 */
+				} else if(input_int[2] != (input_int[0] + 1)) {
+					printf(" 行の入力が不正です\n");
+					return 2;	/* 例外終了 */
+				}
 			}
 			break;
 		/* キング 全方向一つ */	
@@ -185,9 +250,96 @@ int move_check(void)
 		case 'Q':
 			/* 上に行く iに前行代入 後行まで探索 列は固定 */
 			if((input_int[0] > input_int[2]) && (input_int[1] == input_int[3])) {
-				for(i = input_int[0] - 1; i > input_int[2]; i--) {
-					if(board[input_int[i]][input_int[1]] != '*') {//移動先までの間が'*'だったらOK
-						printf(" クイーンの移動先が不正です\n");
+				for(i = (input_int[0] - 1); i > input_int[2]; i--) {
+					if(board[i][input_int[1]] != '*') {//移動先までの間が'*'だったらOK
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] == input_int[3])) {
+				for(i = (input_int[0] + 1); i < input_int[2]; i++) {
+					if(board[i][input_int[1]] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 左に行く */
+			else if((input_int[1] > input_int[3]) && (input_int[0] == input_int[2])) {
+				for(j = (input_int[1] - 1); j > input_int[3]; j--) {
+					if(board[input_int[0]][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右に行く */
+			else if((input_int[1] < input_int[3]) && (input_int[0] == input_int[2])) {
+				for(j = (input_int[1] + 1); j < input_int[3]; j++) {
+					if(board[input_int[0]][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 左上に行く */
+			else if((input_int[0] > input_int[2]) && (input_int[1] > input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[0] - input_int[2]) != (input_int[1] - input_int[3])) {
+					printf(" クイーンの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] - 1), j = (input_int[1] - 1); (i > input_int[2]) && (j > input_int[3]); i--, j--) {//i:行 j:列
+					if(board[i][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 左下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] > input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[2] - input_int[0]) != (input_int[1] - input_int[3])) {
+					printf(" クイーンの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] + 1), j = (input_int[1] - 1); (i < input_int[2]) && (j > input_int[3]); i++, j--) {
+					if(board[i][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右上に行く */
+			else if((input_int[0] > input_int[2]) && (input_int[1] < input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[0] - input_int[2]) != (input_int[3] - input_int[1])) {
+					printf(" クイーンの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] - 1), j = (input_int[1] + 1); (i > input_int[2]) && (j < input_int[3]); i--, j++) {
+					if(board[i][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] < input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[2] - input_int[0]) != (input_int[3] - input_int[1])) {
+					printf(" クイーンの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] + 1), j = (input_int[1] + 1); (i < input_int[2]) && (j < input_int[3]); i++, j++) {
+					if(board[i][j] != '*') {
+						printf(" クイーンの移動先が不正です（衝突）\n");
 						return 2;	/* 例外終了 */
 					}
 				}
@@ -195,15 +347,145 @@ int move_check(void)
 			break;
 		/* ビショップ 斜め方向どこまでも（駒は越せない） */	
 		case 'B':
-		
+			/* 左上に行く */
+			if((input_int[0] > input_int[2]) && (input_int[1] > input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[0] - input_int[2]) != (input_int[1] - input_int[3])) {
+					printf(" ビショップの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか i:行 j:列 ループ条件は一つでいいが一応書いておく */
+				for(i = (input_int[0] - 1), j = (input_int[1] - 1); (i > input_int[2]) && (j > input_int[3]); i--, j--) {
+					if(board[i][j] != '*') {
+						printf(" ビショップの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 左下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] > input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[2] - input_int[0]) != (input_int[1] - input_int[3])) {
+					printf(" ビショップの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] + 1), j = (input_int[1] - 1); (i < input_int[2]) && (j > input_int[3]); i++, j--) {
+					if(board[i][j] != '*') {
+						printf(" ビショップの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右上に行く */
+			else if((input_int[0] > input_int[2]) && (input_int[1] < input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[0] - input_int[2]) != (input_int[3] - input_int[1])) {
+					printf(" ビショップの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] - 1), j = (input_int[1] + 1); (i > input_int[2]) && (j < input_int[3]); i--, j++) {
+					if(board[i][j] != '*') {
+						printf(" ビショップの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] < input_int[3])) {
+				/* 斜め移動では 移動後 - 移動前 が等しかったら正しい */
+				if((input_int[2] - input_int[0]) != (input_int[3] - input_int[1])) {
+					printf(" ビショップの移動先が不正です（ルート外）\n");
+					return 2;	/* 例外終了 */
+				}
+				/* 移動先まで駒にぶつからないかどうか */
+				for(i = (input_int[0] + 1), j = (input_int[1] + 1); (i < input_int[2]) && (j < input_int[3]); i++, j++) {
+					if(board[i][j] != '*') {
+						printf(" ビショップの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
 			break;
 		/* ナイト 全方向桂馬（駒を越せる） */	
 		case 'N':
-			
-			break;
+			/* 上左 */
+			if((input_int[2] == (input_int[0] - 2)) && (input_int[3] == input_int[1] - 1)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 上右 */
+			else if((input_int[2] == (input_int[0] - 2)) && (input_int[3] == input_int[1] + 1)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 下左 */
+			else if((input_int[2] == (input_int[0] + 2)) && (input_int[3] == input_int[1] - 1)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 下右 */
+			else if((input_int[2] == (input_int[0] + 2)) && (input_int[3] == input_int[1] + 1)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 左上 */
+			else if((input_int[2] == (input_int[0] - 1)) && (input_int[3] == input_int[1] - 2)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 左下 */
+			else if((input_int[2] == (input_int[0] + 1)) && (input_int[3] == input_int[1] - 2)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 右上 */
+			else if((input_int[2] == (input_int[0] - 1)) && (input_int[3] == input_int[1] + 2)) {
+				return 0;	/* 正常終了 */
+			}
+			/* 右下 */
+			else if((input_int[2] == (input_int[0] + 1)) && (input_int[3] == input_int[1] + 2)) {
+				return 0;	/* 正常終了 */
+			}
+			/* それ以外は無効 */
+			else {
+				printf(" ナイトの移動先が不正です\n");
+				return 2;	/* 例外終了 */
+			}
+			//break;
 		/* ルーク 縦横どこまでも（駒は越せない） */	
 		case 'R':
-		
+			/* 上に行く iに前行代入 後行まで探索 列は固定 */
+			if((input_int[0] > input_int[2]) && (input_int[1] == input_int[3])) {
+				for(i = (input_int[0] - 1); i > input_int[2]; i--) {
+					if(board[i][input_int[1]] != '*') {//移動先までの間が'*'だったらOK
+						printf(" ルークの移動先が不正です\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 下に行く */
+			else if((input_int[0] < input_int[2]) && (input_int[1] == input_int[3])) {
+				for(i = (input_int[0] + 1); i < input_int[2]; i++) {
+					if(board[i][input_int[1]] != '*') {
+						printf(" ルークの移動先が不正です\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 左に行く */
+			else if((input_int[1] > input_int[3]) && (input_int[0] == input_int[2])) {
+				for(j = (input_int[1] - 1); j > input_int[3]; j--) {
+					if(board[input_int[0]][j] != '*') {
+						printf(" ルークの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
+			/* 右に行く */
+			else if((input_int[1] < input_int[3]) && (input_int[0] == input_int[2])) {
+				for(j = (input_int[1] + 1); j < input_int[3]; j++) {
+					if(board[input_int[0]][j] != '*') {
+						printf(" ルークの移動先が不正です（衝突）\n");
+						return 2;	/* 例外終了 */
+					}
+				}
+			}
 			break;
 			
 		default:
@@ -218,6 +500,30 @@ int move_check(void)
 /* 駒移動 */
 int move_chess(void)
 {	
+	/* 大文字に変えていた駒を小文字に戻す */
+	if(flag_turn == 2) {
+		switch(before) {
+			case 'P':
+				before = 'p';
+				break;
+			case 'K':
+				before = 'k';
+				break;
+			case 'Q':
+				before = 'q';
+				break;
+			case 'B':
+				before = 'b';
+				break;
+			case 'N':
+				before = 'n';
+				break;
+			case 'R':
+				before = 'r';
+				break;
+		}
+	}
+
 	/*指定した駒を移動させる */
 	board[input_int[2]][input_int[3]] = before;
 	
@@ -226,8 +532,11 @@ int move_chess(void)
 	
 	/* 勝ち 終了条件 */
 	if(after == 'k') {
-		printf("\n\n あなたの勝利です\n\n");
-		return 1;	/* 勝利 */
+		printf("\n\n player1の勝利です\n\n");
+		return 1;	/* player1勝利 */
+	} else if(after == 'K') {
+		printf("\n\n player2の勝利です\n\n");
+		return 1;	/* player2勝利 */
 	}
 	
 	return 0;
@@ -256,10 +565,18 @@ int main(void)
 	//flag = 3 ヘルプ
 	
 	/* 開始コメント */
-	printf(" 開始します\n");
+	printf( " 開始します\n"
+			" 'h':ヘルプ\n");
 	
 	/* 'q'を押すか、対局終了までループ */
 	for(;flag != 1;) {
+		
+		/* ターン決定 */
+		if(turn_p1 == turn_p2) {
+			flag_turn = 1;	/* 先手player1 */
+		} else {
+			flag_turn = 2;	/* 後手player2 */
+		}
 		
 		/* 盤面を出力 */
 		print_board();
@@ -281,14 +598,20 @@ int main(void)
 		if(flag == 0) {
 			/* 駒移動 */
 			flag = move_chess();
-			turn_p1++;	/* 駒が動いたらターンが進む */
+			/* ターン数経過 */
+			if(flag_turn == 1) {
+				turn_p1++;
+			} else if(flag_turn == 2) {
+				turn_p2++;
+			}
 		} else if(flag == 3) {
 			/* ヘルプ */
 			help();
 		}
 		
 		/* ターン数表示 */
-		printf(" ターン : %d \n", turn_p1);
+		printf(" player1 : %d ターン目\n", turn_p1);
+		printf(" player2 : %d ターン目\n", turn_p2);
 	}
 	
 	/* 終了コメント */
@@ -305,4 +628,6 @@ int main(void)
  * 9.28 例外処理
  * 9.29 駒 player1:大文字 player2:小文字
  * 		駒の動きのルール実装
+ * 9.30 駒の動きのルール実装
+ * 		ターン制実装
  */
